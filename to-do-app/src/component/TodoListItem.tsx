cb31993c2e854fcb9fa6c2601c042dd766a206e8
DESKTOP-FK2FMAS\22cun <22cun2@naver.com>
2023-08-26 11:36:02 +0900

    insert

---
+++ to-do-app/src/component/TodoListItem.tsx
@@ -0,0 +1,7 @@
+import { MdCheckBoxOutlineBlank, MdCheckBox, MdRemoveCircleOutline } from "react-icons/md";
+
+const TodoListItem : React.FC = () => {
+    return (<div className="TodoListItem">
+        <div className="checkbox"></div>
+    </div>);
+};
